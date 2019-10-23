155793552ed761e7731e3360d13286d92ba1fa18
Ubuntu <ubuntu@joseph-test-build.55m4peoxhotudh0u40tv5eyhhb.xx.internal.cloudapp.net>
2019-10-23 22:02:36 +0000

    Print to stdout that the filter was run

--- echo2.cc
+++ echo2.cc
@@ -5,11 +5,14 @@
 
 #include "common/common/assert.h"
 
+#include <iostream>
+
 namespace Envoy {
 namespace Filter {
 
 Network::FilterStatus Echo2::onData(Buffer::Instance& data, bool) {
   ENVOY_CONN_LOG(trace, "echo: got {} bytes", read_callbacks_->connection(), data.length());
+  std::cout << "Filter example code being executed" << std::endl;
   read_callbacks_->connection().write(data, false);
   return Network::FilterStatus::StopIteration;
 }
